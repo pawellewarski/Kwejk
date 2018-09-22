70cbf0706220b83f15e9ba2ee044254964460b3c
pawellewarski <pawellewarski@gmail.com>
2018-09-22 14:57:21 +0200

    gif class added

---
+++ src/main/java/pl/akademiakodu/model/gif.java
@@ -0,0 +1,37 @@
+package pl.akademiakodu.model;
+
+public class gif {
+    private String name;
+    private String username;
+    private Boolean favorite;
+
+    public gif(String name, String username, Boolean favorite) {
+        this.name = name;
+        this.username = username;
+        this.favorite = favorite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Boolean favorite) {
+        this.favorite = favorite;
+    }
+}
