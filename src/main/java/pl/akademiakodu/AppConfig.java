35106b34ffa62c66473712e322b1ba2e1d1f1abf
pawellewarski <pawellewarski@gmail.com>
2018-09-22 13:01:46 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/AppConfig.java
@@ -0,0 +1,20 @@
+package pl.akademiakodu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+
+@ComponentScan
+@EnableAutoConfiguration
+public class AppConfig {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppConfig.class, args);
+
+    }
+
+}
