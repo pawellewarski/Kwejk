5741ab9c1e6a1ecbf67fa3b0f07165017663c610
pawellewarski <pawellewarski@gmail.com>
2018-09-22 15:29:39 +0200

    gif controller fixed

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -3,7 +3,6 @@ package pl.akademiakodu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import pl.akademiakodu.model.gif;
 
 @Controller
@@ -13,8 +12,6 @@ public class GifController {
     public String listGifs(){
         return "home";
     }
-
-
 
 
 }
