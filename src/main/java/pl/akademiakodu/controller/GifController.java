6cd111a071a91c74f3add8a7fdf381a2b5b23bf6
pawellewarski <pawellewarski@gmail.com>
2018-09-22 15:24:27 +0200

    git controller added

--- src/main/java/pl/akademiakodu/controller/GifController.java
+++ src/main/java/pl/akademiakodu/controller/GifController.java
@@ -1,8 +1,10 @@
 package pl.akademiakodu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.akademiakodu.model.gif;
 
 @Controller
 public class GifController {
@@ -11,5 +13,13 @@ public class GifController {
     public String listGifs(){
         return "home";
     }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        gif gif = new gif("domino","A",true);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
 
 }
